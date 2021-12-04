b0194e290a2c386aeb3c1172dd39f2e7499d9219
Wasif Nadeem <wasif1607@gmail.com>
2021-12-04 17:45:59 +0530

    added at home

--- rShift.c
+++ rShift.c
@@ -20,12 +20,12 @@ int showArr(int arr[], int size)
     {
         printf("%d ", arr[i]);
     }
-    printf("\n");
+    printf("\n\n");
 }
 
-int *rShift(int arr[], int size, int shiftSize)
+int *rShift(int arr2[], int size, int shiftSize)
 {
-    int j = 0;
+    // int j = 0;
 
     for (int i = 0; i < size; i++)
     {
@@ -33,28 +33,52 @@ int *rShift(int arr[], int size, int shiftSize)
         int temp;
         if (i < mag)
         {
-            temp = arr[mag - i - 1];
-            arr[size - i - 1] = temp;
+            arr2[size - i - 1] = arr2[mag - i - 1];
         }
     }
-    return arr;
+    return arr2;
+}
+
+int * final(int arr[], int arr2[], int size, int shiftSize)
+{
+    for (int i = 0; i < size; i++)
+    {
+        int mag = (size - shiftSize);
+        if (i >= mag)
+        {
+            arr2[i - mag] = arr[i];
+        }
+    }
+    return arr2;
 }
 
 int main()
 {
-    int size, shiftSize, *shifted;
+    int size, shiftSize;
     int *n;
     printf("Enter the size of your array: ");
     scanf("%d", &size);
 
-    printf("The the times of right shift: ");
+    printf("The no. times of right shift: ");
     scanf("%d", &shiftSize);
 
     int arr[size];
+    int m[size];
+    // int fin[size];
 
-    n = addArr(arr, size);
+    n = addArr(arr, size); // n is keep on updating, So, I needed to create its copy as m.
+    for (int i = 0; i < size; i++)
+    {
+        m[i] = n[i];
+    }
+    printf("\n");
+    printf("Your array: \n");
     showArr(n, size);
-    shifted = rShift(n, size, shiftSize);
-    showArr(shifted, size);
+    n = rShift(n, size, shiftSize); // arr2
+    // showArr(n, size);  // Partial shift
+    n = final(m, n, size, shiftSize);
+    printf("So, The final output is: \n");
+    showArr(n, size); // Completed.
+
     return 0;
 }
