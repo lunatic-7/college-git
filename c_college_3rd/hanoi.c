af9c02935149866d226f955d6facd8e3c507e68d
Wasif Nadeem <wasif1607@gmail.com>
2022-09-26 20:44:31 +0530

    hanoi

---
+++ c_college_3rd/hanoi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    printf("Move disk %d from rod %c to rod %c\n", n, from_rod, to_rod);
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+}
+
+// Driver code
+int main()
+{
+    int N;
+
+    printf("Enter the no. of disks: ");
+    scanf("%d", &N);
+
+    // A, B and C are names of rods
+    towerOfHanoi(N, 'A', 'C', 'B');
+    return 0;
+}
